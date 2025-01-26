d71f3f7fa5d4631acd4a849a9a0a13b7f9ad2dd9
Adrian Pionka <adrian@pionka.com>
2025-01-26 11:13:48 +0100

    init brewery jms service

---
+++ src/main/java/pl/excellentapp/brewery/beer/jms/breweryjms/BreweryJmsApplication.java
@@ -0,0 +1,13 @@
+package pl.excellentapp.brewery.beer.jms.breweryjms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreweryJmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BreweryJmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/excellentapp/brewery/beer/jms/breweryjms/BreweryJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.excellentapp.brewery.beer.jms.breweryjms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreweryJmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
