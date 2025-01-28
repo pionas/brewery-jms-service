c719af7a20a43e7d9a0130cdd8233ea4b068d54a
Adrian Pionka <adrian@pionka.com>
2025-01-28 16:34:36 +0100

    add object mapper into message converter

--- src/main/java/pl/excellentapp/brewery/beer/jms/config/JmsConfig.java
+++ src/main/java/pl/excellentapp/brewery/beer/jms/config/JmsConfig.java
@@ -1,5 +1,6 @@
 package pl.excellentapp.brewery.beer.jms.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
@@ -7,16 +8,17 @@ import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.jms.support.converter.MessageType;
 
 @Configuration(proxyBeanMethods = false)
-public class JmsConfig {
+class JmsConfig {
 
     public static final String MY_QUEUE = "my-hello-world";
     public static final String MY_SEND_RCV_QUEUE = "replybacktome";
 
     @Bean
-    public MessageConverter messageConverter() {
+    MessageConverter messageConverter(ObjectMapper objectMapper) {
         MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
         converter.setTargetType(MessageType.TEXT);
         converter.setTypeIdPropertyName("_type");
+        converter.setObjectMapper(objectMapper);
         return converter;
     }
 }
